3a51593769ca20b82317bf3fe312b7e569625de9
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2022-04-11 00:10:41 -0300

    add compiled directory file

---
+++ dist/index.d31a8512.js
@@ -0,0 +1,8 @@
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq)=>{
+    faq.addEventListener("click", ()=>{
+        faq.classList.toggle("active");
+    });
+});
+
+//# sourceMappingURL=index.d31a8512.js.map
