259490d7f000f2ad43d3e5913187dc8a7617f1f6
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2022-04-11 00:13:51 -0300

    tweaking some things

--- dist/index.d31a8512.js
+++
@@ -1,8 +0,0 @@
-const faqs = document.querySelectorAll(".faq");
-faqs.forEach((faq)=>{
-    faq.addEventListener("click", ()=>{
-        faq.classList.toggle("active");
-    });
-});
-
-//# sourceMappingURL=index.d31a8512.js.map
