68a7a7355ebe43496711b50a98c9fb48d79aadfa
Daniel Freitas Ferreira <danielfferreirabr@gmail.com>
2022-04-24 14:20:47 -0300

    applying scroll reveal index effect

---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,29 @@
+window.sr = ScrollReveal({
+  reset: true
+});
+
+sr.reveal('.challenges__title', { 
+  duration: 2000
+});
+
+sr.reveal('.brief-challenges', { 
+  duration: 1500,
+  rotate: { x: 0, y: 80, z: 0 } 
+});
+
+sr.reveal('.phrase-gates__description', { 
+  duration: 2000 
+});
+
+sr.reveal('.practive__description', { 
+  duration: 2500 
+});
+
+sr.reveal('.questions__title, .questions-details', { 
+  duration: 2500,
+  rotate: { x: 10, y: 100, z: 0 } 
+});
+
+sr.reveal('.faq__question', { 
+  duration: 2500 
+});
